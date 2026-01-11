5a07a7b94ce7f14ca899366e6f4efcd7224987e4
Tunar <139899544+TunarJamalov@users.noreply.github.com>
2026-01-11 18:12:19 +0400

    Add Vercel Analytics 2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import { languages, countries, ui, categories, contentData, globalFaqData, globalResourcesData, glossary, toolOfTheWeek } from './data.js'
 
 // Initialize Vercel Analytics
 inject();
+injectSpeedInsights();
 
 // State
 const state = {
